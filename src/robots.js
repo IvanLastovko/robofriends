37b46f0364f545a2586c7608b8c1bf20df1d7bab
IvanLastovko <40537180+IvanLastovko@users.noreply.github.com>
2020-07-26 19:59:46 +0300

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Ivan Lastovko',
+        username: 'Bret',
+        email: 'sincereapril.biz'
+    },
+    {
+        id: 2,
+        name: 'Alex Kotsetov',
+        username: 'Alex',
+        email: 'korsetov@mail.ru'
+    },
+    {
+        id: 3,
+        name: 'Andres Holtsman',
+        username: 'andres',
+        email: 'andres@taltech.ee'
+    },
+    {
+        id: 4,
+        name: 'Edvard Ehiloo',
+        username: 'BMW',
+        email: 'mashina@bmw.ru'
+    },
+    {
+        id: 5,
+        name: 'Vova Shulzik',
+        username: 'vova',
+        email: 'volodja@vova.com'
+    },
+    {
+        id: 6,
+        name: 'Alex Poltoracky',
+        username: 'ljoha',
+        email: 'ericsson@ericsson.ee'
+    },
+    {
+        id: 7,
+        name: 'Andres Kokurin',
+        username: 'mr_prezident',
+        email: 'andrei@kokurin.ee'
+    },
+    {
+        id: 8,
+        name: 'Sergei Rassk',
+        username: 'serjoga',
+        email: 'serjoga@narva.ru'
+    }
+]
