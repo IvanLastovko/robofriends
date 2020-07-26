0673c150a03f9af83fa6b198015a3da012ca417f
lastovko97@gmail.com <lastovko97@gmail.com>
2020-07-26 20:27:26 +0300

    Updates

---
+++ precache-manifest.ea349c5c4569f62703f1cc566e16c4cb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "060a9a16011f9eac3996b056b4197338",
+    "url": "/lohotroniki/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/lohotroniki/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7d9be7d5ebde34d12e6b",
+    "url": "/lohotroniki/static/css/main.77f5e7bc.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/lohotroniki/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lohotroniki/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d9be7d5ebde34d12e6b",
+    "url": "/lohotroniki/static/js/main.810612ae.chunk.js"
+  },
+  {
+    "revision": "398aa83611863c93cd73",
+    "url": "/lohotroniki/static/js/runtime-main.f53a45c2.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/lohotroniki/static/media/SEGA.12a3db8b.woff"
+  }
+]);
